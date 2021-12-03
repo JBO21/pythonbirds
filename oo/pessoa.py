d06a839fe790d0683cdfbe73360ca602d8f7f9bc
JBO21 <jacksonjbo@gmail.com>
2021-12-03 18:04:17 -0300

    Criado atributo de instacia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,18 @@
 class Pessoa:
+    def __init__(self, nome = None, idade = 37):
+        self.nome = nome
+
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('zÉ')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Jackson'
+    print(p.nome)
+    print(p.idade)
