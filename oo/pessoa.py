122cb9070b4ecfb3474cdc589b66ff3756f14451
JBO21 <jacksonjbo@gmail.com>
2021-12-21 16:10:08 -0300

    Criado e removido atributo dinamico de objeto tipo pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -18,3 +18,8 @@ if __name__ == '__main__':
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome)
+    luciano.sobrenome = 'Santos'
+    del luciano.filhos
+    print(luciano.__dict__)
+    print(ze.__dict__)
+
