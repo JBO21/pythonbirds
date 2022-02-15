86701411892a6ba7b22ce8a5d5f6d38f9a1c5aa3
JBO21 <jackson.jbo@gmail.com>
2022-02-15 17:46:11 -0300

    Implementada direção

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -11,6 +11,15 @@ class Pessoa:
         return f'Olá {id(self)}'
 
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
+
 if __name__ == '__main__':
     ze = Pessoa(nome='Zé')
     luciano = Pessoa(ze, nome='Luciano')
@@ -32,4 +41,6 @@ if __name__ == '__main__':
     print(luciano.olhos)
     print(ze.olhos)
     print(id(Pessoa.olhos), id(luciano.olhos), id(ze.olhos))
-
+    print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
+    
